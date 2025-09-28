f12cbb462ea015d944b8ea0f4b6810da10f4ae8d
SuperGuirlande <dsj.gamemaster@gmail.com>
2025-09-28 23:22:15 +0200

    SEO

--- project/main/urls.py
+++ project/main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, robots_txt
+from .views import index, robots_txt, sitemap_xml
 from django.conf import settings
 from django.conf.urls.static import static 
 
@@ -8,6 +8,7 @@ app_name = 'main'
 urlpatterns = [
     path('', index, name='index'),
     path('robots.txt', robots_txt, name='robots_txt'),
+    path('sitemap.xml', sitemap_xml, name='sitemap_xml'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- project/main/views.py
+++ project/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponseRedirect, HttpResponse
 from django.views.decorators.cache import cache_page
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import BlogSitemap, CategorySitemap, BlogIndexSitemap, MainSitemap
 from contact_management.forms import ContactForm
 from django.contrib import messages
 import os
@@ -47,4 +49,18 @@ def robots_txt(request):
         return HttpResponse(content, content_type='text/plain')
     except FileNotFoundError:
         return HttpResponse('User-agent: *\nDisallow: /', content_type='text/plain')
+
+
+# Configuration des sitemaps
+sitemaps = {
+    'main': MainSitemap,
+    'blog_index': BlogIndexSitemap,
+    'blog_articles': BlogSitemap,
+    'blog_categories': CategorySitemap,
+}
+
+@cache_page(60 * 60 * 12)  # Cache 12h
+def sitemap_xml(request):
+    """Vue pour servir la sitemap avec le bon Content-Type"""
+    return sitemap(request, sitemaps, template_name='sitemap.xml', content_type='application/xml')
 
