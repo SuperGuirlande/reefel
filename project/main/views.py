efd9b1234332592797189eef2749929a6bf1fe3c
SuperGuirlande <dsj.gamemaster@gmail.com>
2025-10-01 16:06:43 +0200

    Version finale

--- project/main/views.py
+++ project/main/views.py
@@ -93,87 +93,64 @@ sitemaps = {
     'blog_categories': CategorySitemap,
 }
 
-@cache_page(60 * 60 * 12)  # Cache 12h
 def sitemap_xml(request):
     """Vue pour servir la sitemap avec le bon Content-Type"""
-    try:
-        from django.contrib.sites.models import Site
-        from django.conf import settings
-        from django.http import HttpResponse
-        from django.template.loader import render_to_string
-        from django.utils import timezone
-        
-        # Construire l'URL de base correcte
-        if settings.DEBUG:
-            base_url = f"{request.scheme}://{request.get_host()}"
-        else:
-            try:
-                site = Site.objects.get_current()
-                base_url = f"https://{site.domain}"
-            except:
-                # Fallback si le site n'est pas configuré
-                base_url = f"https://www.reefel.fr"
-        
-        # Générer le contenu XML manuellement pour contrôler les URLs
-        urls = []
-        
-        # Page d'accueil
-        urls.append({
-            'loc': f"{base_url}/",
-            'lastmod': timezone.now().strftime('%Y-%m-%d'),
-            'changefreq': 'weekly',
-            'priority': '1.0'
-        })
-        
-        # Blog index
-        urls.append({
-            'loc': f"{base_url}/blog/",
-            'lastmod': timezone.now().strftime('%Y-%m-%d'),
-            'changefreq': 'daily',
-            'priority': '0.9'
-        })
-        
-        # Articles de blog
-        try:
-            from blog.models import Post
-            for post in Post.objects.filter(published=True).order_by('-updated_at')[:100]:  # Limiter à 100 articles
-                urls.append({
-                    'loc': f"{base_url}/blog/article/{post.slug}/",
-                    'lastmod': post.updated_at.strftime('%Y-%m-%d'),
-                    'changefreq': 'weekly',
-                    'priority': '0.9'
-                })
-        except Exception as e:
-            # Si erreur avec les articles, continuer sans eux
-            pass
-        
-        # Catégories
-        try:
-            from blog.models import Category
-            for category in Category.objects.all().order_by('name')[:50]:  # Limiter à 50 catégories
-                urls.append({
-                    'loc': f"{base_url}/blog/categorie/{category.slug}/",
-                    'changefreq': 'monthly',
-                    'priority': '0.6'
-                })
-        except Exception as e:
-            # Si erreur avec les catégories, continuer sans elles
-            pass
-        
-        context = {'urls': urls}
-        xml_content = render_to_string('main/sitemap.xml', context)
-        
-        return HttpResponse(xml_content, content_type='application/xml')
-        
-    except Exception as e:
-        # En cas d'erreur, retourner un sitemap minimal
-        minimal_sitemap = f"""<?xml version="1.0" encoding="UTF-8"?>
+    from django.http import HttpResponse
+    from django.utils import timezone
+    
+    # Construire l'URL de base correcte
+    if 'www.reefel.fr' in request.get_host() or 'reefel.fr' in request.get_host():
+        base_url = "https://www.reefel.fr"
+    elif 'example.com' in request.get_host():
+        base_url = "https://www.reefel.fr"  # Forcer le bon domaine même si configuré avec example.com
+    else:
+        base_url = f"{request.scheme}://{request.get_host()}"
+    
+    # Générer le contenu XML directement
+    xml_content = f"""<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-        <loc>https://www.reefel.fr/</loc>
+        <loc>{base_url}/</loc>
+        <lastmod>{timezone.now().strftime('%Y-%m-%d')}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>1.0</priority>
     </url>
+    <url>
+        <loc>{base_url}/blog/</loc>
+        <lastmod>{timezone.now().strftime('%Y-%m-%d')}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.9</priority>
+    </url>"""
+    
+    # Ajouter les articles de blog
+    try:
+        from blog.models import Post
+        for post in Post.objects.filter(published=True).order_by('-updated_at')[:50]:
+            xml_content += f"""
+    <url>
+        <loc>{base_url}/blog/article/{post.slug}/</loc>
+        <lastmod>{post.updated_at.strftime('%Y-%m-%d')}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.9</priority>
+    </url>"""
+    except:
+        pass
+    
+    # Ajouter les catégories
+    try:
+        from blog.models import Category
+        for category in Category.objects.all().order_by('name')[:20]:
+            xml_content += f"""
+    <url>
+        <loc>{base_url}/blog/categorie/{category.slug}/</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.6</priority>
+    </url>"""
+    except:
+        pass
+    
+    xml_content += """
 </urlset>"""
-        return HttpResponse(minimal_sitemap, content_type='application/xml')
+    
+    return HttpResponse(xml_content, content_type='application/xml')
 
